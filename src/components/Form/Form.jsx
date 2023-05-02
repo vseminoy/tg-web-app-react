1648e7fd041acbf3c302813fc4ccd8b65949d72e
semina.v@itbizon.com <0bmNrYu@VWAqD~>
2023-05-02 15:52:44 +0300

    form, send data

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -32,6 +32,7 @@ const Form = () => {
             street,
             subject
         };
+        console.log(JSON.stringify(data));
         tg.sendData(JSON.stringify(data));
     },[country, street, subject]);
 
