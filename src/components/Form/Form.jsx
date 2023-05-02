fbf16d579ab612e33219e4c02f3afe25b27d0ed8
semina.v@itbizon.com <0bmNrYu@VWAqD~>
2023-05-02 15:20:45 +0300

    form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -34,10 +34,10 @@ const Form = () => {
     },[country,street])
 
     return (
-        <div class={'form'}>
+        <div className={'form'}>
             <h3>Введите ваши данные</h3>
-            <input className={'input'} type="text" placeholder={'Страна'} value={country} onClick={onChangeCountry} />
-            <input className={'input'} type="text" placeholder={'Улица'} value={street} onClick={onChangeStreet} />
+            <input className={'input'} type="text" placeholder={'Страна'} value={country} onChange={onChangeCountry} />
+            <input className={'input'} type="text" placeholder={'Улица'} value={street} onChange={onChangeStreet} />
             <select value={subject} onChange={onChangeSubject} className={'select'} >
                 <option value={'physical'}>Физ.лицо</option>
                 <option value={'legal'}>Юр.лицо</option>
