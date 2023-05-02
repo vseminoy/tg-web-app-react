43b9141966464895b4f989e9457593978a82fb00
semina.v@itbizon.com <0bmNrYu@VWAqD~>
2023-05-02 17:14:12 +0300

    form, send data

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ProductItem.css'
+
+const ProductItem = ({product, className, onAdd}) => {
+    const onAddHandler = () => {
+        onAdd(product);
+    }
+    return (
+        <div className={'product ' + className}>
+            <div className={'img'} />
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
+            <Button classsName={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
+        </div>
+    );
+};
+
+export default ProductItem;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProductList.css'
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+
+const products = [
+    {id: 1, title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: 2, title: 'Джинсы 2', price: 4500, description: 'Зеленого цвета, палацо'},
+    {id: 3, title: 'Куртка', price: 12000, description: 'Синего цвета, теплая'},
+    {id: 4, title: 'Куртка 2', price: 8300, description: 'Зеленого цвета, деимсезон'},
+]
+
+const getTotalPrice = (items=[]) => {
+    return items.reduce((acc,item) => {
+        return acc += item.price;
+    },0);
+}
 
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg} = useTelegram();
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id==product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id!==product.id);
+        }else {
+            newItems = [...newItems,product];
+        }
+
+        setAddedItems(newItems);
+
+        if(newItems.length === 0){
+            tg.MainButton.hide();
+        }else{
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            });
+        }
+    }
+
     return (
-        <div>
-            PRODUCTLIST
+        <div className={'list'}>
+            {products.map( item => (
+                <ProductItem product={product} onAdd={onAdd} className={'item'}/>
+            ))}
         </div>
     );
 };
